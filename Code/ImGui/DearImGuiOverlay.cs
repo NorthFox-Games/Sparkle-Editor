b95dd1590889127242e491e6b6eeee35f3a8c49c
ВсегдаВыключенныйПользователь <113923625+everyofflineuser@users.noreply.github.com>
2024-09-24 11:41:47 +0900

    DearImGui import
    Co-Authored-By: copper <79734708+copperdevs@users.noreply.github.com>

---
+++ Code/ImGui/DearImGuiOverlay.cs
@@ -0,0 +1,28 @@
+﻿using CopperDevs.DearImGui;
+using CopperDevs.DearImGui.Renderer.Raylib;
+using Sparkle.CSharp.Overlays;
+
+namespace Sparkle_Editor.Code.ImGui;
+
+public class DearImGuiOverlay : Overlay
+{
+    public DearImGuiOverlay(string name) : base(name)
+    {
+        CopperImGui.Setup<RlImGuiRenderer>(true, true);
+        CopperImGui.ShowDearImGuiAboutWindow = true;
+        CopperImGui.ShowDearImGuiDemoWindow = true;
+        CopperImGui.ShowDearImGuiMetricsWindow = true;
+        CopperImGui.ShowDearImGuiDebugLogWindow = true;
+        CopperImGui.ShowDearImGuiIdStackToolWindow = true;
+    }
+
+    protected override void Draw()
+    {
+        CopperImGui.Render();
+    }
+
+    ~DearImGuiOverlay()
+    {
+        CopperImGui.Shutdown();
+    }
+}
