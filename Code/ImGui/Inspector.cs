134f793c6ca5ff2f523b2483dee8cc0b3e526352
ВсегдаВыключенныйПользователь <113923625+everyofflineuser@users.noreply.github.com>
2024-09-26 09:26:42 +0900

    Update Inspector.cs

--- Code/ImGui/Inspector.cs
+++ Code/ImGui/Inspector.cs
@@ -1,6 +1,8 @@
+using System.Numerics;
 using CopperDevs.DearImGui;
 using CopperDevs.DearImGui.Attributes;
 using CopperDevs.DearImGui.Rendering;
+using ImGuiNET;
 using Sparkle_Editor.Code.Managers;
 
 namespace Sparkle_Editor.Code.ImGui;
@@ -13,12 +15,16 @@ public class Inspector : BaseWindow
         var ent = SelectingManager.SelectedEntity;
          
         if (ent == null) return;
+
+        Vector3 tempPosition = ent.Position;
         
         CopperImGui.Text($"ID: {ent.Id}");
         CopperImGui.Text($"TAG: {ent.Tag}");
+        CopperImGui.HorizontalGroup(() => {ImGuiNET.ImGui.DragFloat("X", ref tempPosition.X);});
+        CopperImGui.HorizontalGroup(() => {ImGuiNET.ImGui.DragFloat("Y", ref tempPosition.Y);}); 
+        CopperImGui.HorizontalGroup(() => {ImGuiNET.ImGui.DragFloat("Z", ref tempPosition.Z);});
         CopperImGui.Separator("Components");
-        CopperImGui.HorizontalGroup(() => {ImGuiNET.ImGui.InputText("X", new byte[16], 16);});
-        CopperImGui.HorizontalGroup(() => {ImGuiNET.ImGui.InputText("Y", new byte[16], 16);}); 
-        CopperImGui.HorizontalGroup(() => {ImGuiNET.ImGui.InputText("Z", new byte[16], 16);});
+
+        ent.Position = tempPosition;
     }
 }
