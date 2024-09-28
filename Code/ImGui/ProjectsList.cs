840474da84655eb78e5f834ebd62295a0314dc59
ВсегдаВыключенныйПользователь <113923625+everyofflineuser@users.noreply.github.com>
2024-09-29 01:58:35 +0900

    feat-starting work on Project Manager

--- Code/ImGui/ProjectsList.cs
+++ Code/ImGui/ProjectsList.cs
@@ -1,22 +1,43 @@
 ﻿using CopperDevs.DearImGui;
 using CopperDevs.DearImGui.Attributes;
 using CopperDevs.DearImGui.Rendering;
+using Sparkle_Editor.Code.Managers;
 
 namespace Sparkle_Editor.Code.ImGui;
 
 [Window("Projects", WindowOpen = true)]
 public class ProjectsList : BaseWindow
 {
+    private bool _newProject = false;
+    
     public override void WindowUpdate()
     {
         base.WindowUpdate();
         
-        CopperImGui.Separator("Welcome to Sparkle Editor");
+        ImGuiNET.ImGui.SetWindowFocus();
+
+        if (_newProject)
+        {
+            CopperImGui.Button("Back", () => {_newProject = false;});
+            CopperImGui.Text("KUKU EPTA NAHUI");
+            return;
+        }
         
+        CopperImGui.Separator("Welcome to Sparkle Editor!");
+
         CopperImGui.HorizontalGroup(
-            () => CopperImGui.Button("New", () => {throw new NotImplementedException();}),
-            () => { CopperImGui.Button("Open", () => {throw new NotImplementedException();}); }); //TODO: center the buttons
+            () => CopperImGui.Button("New", () => { _newProject = true; }),
+            () => CopperImGui.Button("Open", OpenProject)); //TODO: center the buttons
         
         CopperImGui.Separator("Last projects");
+    }
+
+    private void OpenProject()
+    {
+        string? path = ProjectManager.OpenExistProject();
+        
+        if (path == null) return;
+        
+        ImGuiNET.ImGui.CloseCurrentPopup();
     }
 }
---
+++ Code/Managers/ProjectManager.cs
@@ -0,0 +1,16 @@
+﻿using NativeFileDialogNET;
+
+namespace Sparkle_Editor.Code.Managers;
+
+public static class ProjectManager
+{
+    public static string? OpenExistProject()
+    {
+        using var selectFileDialog = new NativeFileDialog()
+            .SelectFile()
+            .AddFilter("Sparkle Editor Project", "seproj"); // Optionally add filters
+
+        DialogResult result = selectFileDialog.Open(out string? output, Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+        return result == DialogResult.Okay ? output : String.Empty;
+    }
+}
