73aee62b050eed81cc5fbd82f5e6975304e92a63
Ivan Timersky <96748043+time2138@users.noreply.github.com>
2024-09-28 19:13:04 +0300

    add ProjectsList.cs window

---
+++ Code/ImGui/ProjectsList.cs
@@ -0,0 +1,20 @@
+﻿using CopperDevs.DearImGui;
+using CopperDevs.DearImGui.Attributes;
+using CopperDevs.DearImGui.Rendering;
+
+namespace Sparkle_Editor.Code.ImGui;
+
+[Window("Projects", WindowOpen = true)]
+public class ProjectsList : BaseWindow
+{
+    public override void WindowUpdate()
+    {
+        base.WindowUpdate();
+        
+        CopperImGui.Separator("Welcome to Sparkle Editor");
+        
+        CopperImGui.HorizontalGroup(
+            () => CopperImGui.Button("New", () => {throw new NotImplementedException();}),
+            () => { CopperImGui.Button("Open", () => {throw new NotImplementedException();}); });
+    }
+}
