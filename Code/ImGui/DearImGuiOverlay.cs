2b7238f9331a3e02ee979817b199e3ac5ed3bea7
ВсегдаВыключенныйПользователь <113923625+everyofflineuser@users.noreply.github.com>
2024-10-05 22:38:29 +0900

    I've come back!

--- Code/ImGui/DearImGuiOverlay.cs
+++ Code/ImGui/DearImGuiOverlay.cs
@@ -1,6 +1,9 @@
 ﻿using CopperDevs.DearImGui;
 using CopperDevs.DearImGui.Renderer.Raylib;
+using Raylib_CSharp.Logging;
+using Sparkle_Editor.Code.Managers;
 using Sparkle.CSharp.Overlays;
+using Logger = Sparkle.CSharp.Logging.Logger;
 
 namespace Sparkle_Editor.Code.ImGui;
 
@@ -14,6 +17,20 @@ public class DearImGuiOverlay : Overlay
         CopperImGui.ShowDearImGuiMetricsWindow = false;
         CopperImGui.ShowDearImGuiDebugLogWindow = false;
         CopperImGui.ShowDearImGuiIdStackToolWindow = false;
+
+        (string, Action)[] MenuActions = new (string, Action)[]
+        {
+            new ValueTuple<string, Action>("Project", () =>
+            {
+                CopperImGui.MenuItem("New", () => Logger.Info("New Project"));
+                CopperImGui.MenuItem("Open", () => Logger.Info(ProjectManager.OpenExistProject()!));
+            }),
+        };
+        
+        CopperImGui.PreRendered += () =>
+        {
+            CopperImGui.MenuBar(true, MenuActions);
+        };
     }
 
     protected override void Draw()
